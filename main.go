27c6e78f53eb290d494a708c7bb6d986fcb836fb
Robert Brünig <rbr@iqusystems.com>
2016-05-14 13:26:53 +0200

    packets.go rewritten

--- main.go
+++ main.go
@@ -45,49 +45,34 @@ func main() {
     // REQ
     req := pft.EncodeReq("file:/tmp/uno.txt")
     fmt.Println("Sent REQ:", req)
-
     fmt.Println("REQ HASH ok:", pft.VerifyPacket(req))
-
     _, resource := pft.DecodeReq(req)
     fmt.Println("Received REQ:", resource)
-    //
 
     // PUSH
-    _,push := pft.EncodePush("file:/tmp/dos.txt")
+    push := pft.EncodePush("file:/tmp/dos.txt")
     fmt.Println("Sent PUSH:", push)
 
-    _, received_push, pt := pft.DecodePush(push)
-    fmt.Println("Received PUSH:", received_push, pt)
-    //
-
     //NACK
-    _,nack := pft.EncodeNack()
+    nack := pft.EncodeNack()
     fmt.Println("Sent NACK:", nack)
+    fmt.Println("NACK ok:", len(nack) == 17 && nack[16] == 3 && pft.VerifyPacket(nack))
 
-    _, received_nack := pft.DecodeNack(nack)
-    fmt.Println("Received REQ:", received_nack)
-    //
 
     //PUSH-ACK
-    _,push_ack := pft.EncodePushAck()
+    push_ack := pft.EncodePushAck()
     fmt.Println("Sent PUSH-ACK:", push_ack)
 
-    _, received_push_ack := pft.DecodePushAck(push_ack)
-    fmt.Println("Received PUSH-ACK:", received_push_ack)
-
     //RST
-    _,rst := pft.EncodeRst()
+    rst := pft.EncodeRst()
     fmt.Println("Sent RST:", rst)
 
-    _, received_rst := pft.DecodeRst(rst)
-    fmt.Println("Received RST:", received_rst)
-
     //GET
-    _,get := pft.EncodeGet([4]byte{20,23,129,20})
+    get := pft.EncodeGet(123456)
     fmt.Println("Sent GET:", get)
 
-    _, received_get, pt := pft.DecodeGet(get)
-    fmt.Println("Received GET:", received_get, pt)
+    _,index := pft.DecodeGet(get)
+    fmt.Println("Received GET:", index)
 
     //DATA
     data := pft.EncodeData(123456789, []byte{9, 8, 7, 6, 5, 4, 3, 2, 1})
@@ -99,13 +84,16 @@ func main() {
     fmt.Println("Received DATA:", block,  data)
 
     //REQ-ACK
-    _,req_ack := pft.EncodeReqAck([8]byte{3,4,5,6})
+    req_ack := pft.EncodeReqAck(40000000001,
+        []byte{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16})
     fmt.Println("Sent REQ-ACK:", req_ack)
 
-    _, received_req_ack, resource_size, pt := pft.DecodeReqAck(req_ack)
-    fmt.Println("Received REQ-ACK:", received_req_ack, resource_size,  pt)
-
-
+    err, size, hash := pft.DecodeReqAck(req_ack)
+    if err != nil {
+        fmt.Println("error in req ack:", err)
+    } else {
+        fmt.Println("received req ack with size", size, "and hash", hash)
+    }
 }
 
 
--- pft/packets.go
+++ pft/packets.go
@@ -8,109 +8,34 @@ import (
     "errors"
 )
 
-
-type Req struct {
-    F1    [16]byte      // SHA3
-    F2    [1]byte       // Type
-    F3    [495]byte     // RID
-}
-
-type Req_ack struct {
-    F1    [16]byte      // SHA3
-    F2    [1]byte       // Type
-    F3    [8]byte       // Resource Size
-    F4    [32]byte      // Resource Hash
-}
-
-type Req_nack struct {
-    F1    [16]byte      // SHA3
-    F2    [1]byte       // Type
-}
-
-type Push struct {
-    F1    [16]byte      // SHA3
-    F2    [1]byte       // Type
-    F3    [495]byte     // RID
-}
-
-type Push_ack struct {
-    F1    [16]byte      // SHA3
-    F2    [1]byte       // Type
-}
-
-type Get struct {
-    F1    [16]byte      // SHA3
-    F2    [1]byte       // Type
-    F3    [4]byte       // Block index
-}
-
-type Data struct {
-    F1    [16]byte      // SHA3
-    F2    [1]byte       // Type
-    F3    [4]byte       // Block index
-    F4    [491]byte     // Data
-}
-
-type Rst struct {
-    F1    [16]byte      // SHA3
-    F2    [1]byte       // Type
-}
-
-func GetSha3 (p_type [1]byte, field []byte) [16]byte {
-
-    var hash  [16]byte
-    var tmp   []byte
-
-    // Specific field first + Packet_type
-    tmp = append(field[:])
-    tmp = append(tmp, p_type[0])
-
-    h := sha3.Sum256(tmp)
-
-    //truncating
-    for i := 0; i < 16; i++ {
-        hash[i] = h[i]
-    }
-
-    return hash
-}
-
-
-func ResourceHash (p_type [1]byte, resource_size [8]byte) [32]byte {
-
-    var res_hash  [32]byte
-    var tmp   []byte
-
-    // Specific field first + Packet_type
-    tmp = append(resource_size[:])
-    tmp = append(tmp, p_type[0])
-
-    h := sha3.Sum256(tmp)
-
-    //truncating
-    for i := 0; i < 32; i++ {
-        res_hash[i] = h[i]
-    }
-
-    return res_hash
-}
-
-
 // calculates hash and concatenates packet to single []byte
 func MakePacket(packet_type byte, payload []byte) []byte {
-    content := append([]byte{packet_type}, payload...)
+    var content []byte
+    if(payload == nil) {
+        content = []byte{packet_type}
+    } else {
+        content = append([]byte{packet_type}, payload...)
+    }
+
     hash := sha3.Sum256(content)
     return append(hash[:16], content...)
 }
+
 
 func VerifyPacket(packet []byte) bool {
     hash := sha3.Sum256(packet[16:])
     return bytes.Equal(packet[:16], hash[:16])
 }
 
-func ToBigEndian(num uint32) []byte {
+func ToBigEndian32(num uint32) []byte {
     num_big_endian := make([]byte, 4)
     binary.BigEndian.PutUint32(num_big_endian, num)
+    return num_big_endian
+}
+
+func ToBigEndian64(num uint64) []byte {
+    num_big_endian := make([]byte, 8)
+    binary.BigEndian.PutUint64(num_big_endian, num)
     return num_big_endian
 }
 
@@ -127,7 +52,7 @@ func DecodeReq(packet []byte) (error, string) {
 }
 
 func EncodeData (block_index uint32, data_block []byte) []byte {
-    return MakePacket(7, append(ToBigEndian(block_index), data_block...))
+    return MakePacket(7, append(ToBigEndian32(block_index), data_block...))
 }
 
 func DecodeData(packet []byte) (error, uint32, []byte) {
@@ -138,154 +63,58 @@ func DecodeData(packet []byte) (error, uint32, []byte) {
     return nil, binary.BigEndian.Uint32(packet[17:21]), packet[21:]
 }
 
+func EncodeNack () []byte {
+    return MakePacket(3, nil)
+}
 
-
-func EncodeNack () (error, []byte) {
-
-    packet_type := [1]byte{3}
-    hash := GetSha3(packet_type, []byte{3})
-
-    encode := Req_nack{hash, packet_type}
-    buf := new(bytes.Buffer)
-    err := binary.Write(buf, binary.BigEndian, &encode)
-
-    return err, buf.Bytes()
+func EncodePushAck() []byte {
+    return MakePacket(5, nil)
 
 }
 
-func DecodeNack(packet []byte) (error, [1]byte) {
-
-    var decoded Req_nack
-    err := binary.Read(bytes.NewReader(packet), binary.BigEndian, &decoded)
-
-    return err, decoded.F2
+func EncodeRst() []byte {
+    return MakePacket(8, nil)
 }
 
-func EncodePushAck () (error, []byte) {
 
-    packet_type := [1]byte{5}
-    hash := GetSha3(packet_type, []byte{5})
-
-    encode := Push_ack{hash, packet_type}
-    buf := new(bytes.Buffer)
-    err := binary.Write(buf, binary.BigEndian, &encode)
-
-    return err, buf.Bytes()
-
+func EncodePush (rid string) []byte {
+    return MakePacket(4, []byte(rid))
 }
 
-func DecodePushAck(packet []byte) (error, [1]byte) {
-
-    var decoded Push_ack
-    err := binary.Read(bytes.NewReader(packet), binary.BigEndian, &decoded)
-
-    return err, decoded.F2
-}
-
-func EncodeRst () (error, []byte) {
-
-    packet_type := [1]byte{8}
-    hash := GetSha3(packet_type, []byte{8})
-
-    encode := Rst{hash, packet_type}
-    buf := new(bytes.Buffer)
-    err := binary.Write(buf, binary.BigEndian, &encode)
-
-    return err, buf.Bytes()
-
-}
-
-func DecodeRst(packet []byte) (error, [1]byte) {
-
-    var decoded Rst
-    err := binary.Read(bytes.NewReader(packet), binary.BigEndian, &decoded)
-
-    return err, decoded.F2
-}
-
-func EncodePush (rid string) (error, []byte) {
-
-    var rid_bytes   [495]byte
-    packet_type := [1]byte{4}
-    temp := make([]byte, 495)
-
-    if (len(rid) < 495) {
-        // Getting the characters to the fixed size binary array
-        for i := 0; i < len(rid); i++ {
-            rid_bytes[i] = rid[i]
-        }
-        temp = rid_bytes[:]
-        hash := GetSha3(packet_type,  temp)
-
-        encode := Push{hash, packet_type, rid_bytes}
-        buf := new(bytes.Buffer)
-        err := binary.Write(buf, binary.BigEndian, &encode)
-
-        return err, buf.Bytes()
+func DecodePush(packet []byte) (error, string) {
+    if len(packet) <= 17 {
+        return errors.New("packet too short"), ""
     }
 
-    return nil, nil // If the sanity check is not passed
-}
-
-func DecodePush(packet []byte) (error, string, [1]byte) {
-
-    var decoded Push
-    err := binary.Read(bytes.NewReader(packet), binary.BigEndian, &decoded)
-
-    result := string(bytes.Trim(decoded.F3[:495], "\x00"))
-    return err, result, decoded.F2
+    return nil, string(packet[17:])
 
 }
 
 
-func EncodeGet (blockIndex [4]byte) (error, []byte) {
-
-    packet_type := [1]byte{6}
-    temp := make([]byte, 4)
-
-    temp = blockIndex[:]
-    hash := GetSha3(packet_type,  temp)
-
-    encode := Get{hash, packet_type, blockIndex}
-    buf := new(bytes.Buffer)
-    err := binary.Write(buf, binary.BigEndian, &encode)
-
-    return err, buf.Bytes()
+func EncodeGet (blockIndex uint32) []byte {
+    return MakePacket(6, ToBigEndian32(blockIndex))
 
 }
 
-func DecodeGet(packet []byte) (error, [4]byte, [1]byte) {
+func DecodeGet(packet []byte) (error, uint32) {
+    if len(packet) != 21 {
+        return errors.New("packet too short"), 0
+    }
 
-    var decoded Get
-    err := binary.Read(bytes.NewReader(packet), binary.BigEndian, &decoded)
-
-    return err, decoded.F3, decoded.F2
-
+    return nil, binary.BigEndian.Uint32(packet[17:21])
 }
 
 
-func EncodeReqAck (resource_size [8]byte) (error, []byte) {
-
-    packet_type := [1]byte{2}
-    temp := make([]byte, 8)
-
-    temp = resource_size[:]
-    hash := GetSha3(packet_type,  temp)
-
-    resource_hash := ResourceHash(packet_type, resource_size)
-
-    encode := Req_ack{hash, packet_type, resource_size, resource_hash}
-    buf := new(bytes.Buffer)
-    err := binary.Write(buf, binary.BigEndian, &encode)
-
-    return err, buf.Bytes()
+func EncodeReqAck (resource_size uint64, resource_hash []byte) []byte {
+    return MakePacket(2, append(ToBigEndian64(resource_size), resource_hash...))
 }
 
-func DecodeReqAck(packet []byte) (error, [32]byte, [8]byte, [1]byte) {
+func DecodeReqAck(packet []byte) (error, uint64, []byte) {
+    if len(packet) != 17 + 8 + 32 { // 17 byte header, 8 byte resource size, 32 byte resource hash
+        return errors.New("invalid packet length"), 0, nil
+    }
 
-    var decoded Req_ack
-    err := binary.Read(bytes.NewReader(packet), binary.BigEndian, &decoded)
+    resource_size := binary.BigEndian.Uint64(packet[17:25])
 
-    return err, decoded.F4, decoded.F3, decoded.F2
-
+    return nil, resource_size, packet[25:57]
 }
