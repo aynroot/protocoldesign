7c394571f2211acfac1d37616483a5908b328a93
Robert Brünig <rbr@iqusystems.com>
2016-05-16 21:26:25 +0200

    first wave of fixes in download.go (untested). This fixes unchecked gob encode, received and requested index in part file, open/close of download file on each data packet and interface issues.

--- pft/common.go
+++ pft/common.go
@@ -14,7 +14,7 @@ const (
 const (
 	REQ      byte = 1
 	REQ_ACK  byte = 2
-	NACK     byte = 3
+	REQ_NACK byte = 3
 	PUSH     byte = 4
 	PUSH_ACK byte = 5
 	GET      byte = 6
@@ -26,8 +26,19 @@ const DATA_BLOCK_SIZE = 491
 const UDP_BUFFER_SIZE = 512
 
 func CheckError(err error) {
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(1)
-	}
+    if err != nil {
+        fmt.Println("Error: ", err)
+        os.Exit(1)
+    }
 }
+
+func GetFileDir() string {
+    wd, err := os.Getwd()
+    CheckError(err)
+    file_dir := fmt.Sprint("%s/%s", wd, "pft-files")
+    err = os.MkdirAll(file_dir, 644)
+    CheckError(err)
+    return file_dir
+}
+
+
--- pft/download.go
+++ pft/download.go
@@ -1,17 +1,12 @@
 package pft
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strings"
-
-	"bytes"
-	"encoding/gob"
-	"errors"
-	"io/ioutil"
-	"path/filepath"
-	"bufio"
+    "fmt"
+    "log"
+    "os"
+    "encoding/gob"
+    "errors"
+    "path/filepath"
 )
 
 // this packet deals with resource downloads
@@ -19,200 +14,132 @@ import (
 // a partial download file needs to save the rid, the size, the hash and the already downloaded data of the resource
 
 type Download struct {
-	server                string
-	port                  int
-	rid                   string
-	size                  uint64
-	hash                  []byte
-	received_index        uint32
-	requested_index       uint32
-	partial_file_path     string
-	destination_file_path string
+    server          string
+    port            int
+    rid             string
+    size            uint64
+    hash            []byte
+    received_index  uint32
+    requested_index uint32
+    local_file      *os.File
 }
 
-func (this *Download) GobEncode() ([]byte, error) {
-	buffer := new(bytes.Buffer)
-	encoder := gob.NewEncoder(buffer)
-
-	encoder.Encode(this.server)
-	encoder.Encode(this.port)
-	encoder.Encode(this.rid)
-	encoder.Encode(this.size)
-	encoder.Encode(this.hash)
-	encoder.Encode(this.received_index)
-	encoder.Encode(this.requested_index)
-	encoder.Encode(this.partial_file_path)
-	encoder.Encode(this.destination_file_path)
-
-	return buffer.Bytes(), nil
-}
-
-func (this *Download) GobDecode(data []byte) error {
-	buffer := bytes.NewBuffer(data)
-	decoder := gob.NewDecoder(buffer)
-
-	decoder.Decode(&this.server)
-	decoder.Decode(&this.port)
-	decoder.Decode(&this.rid)
-	decoder.Decode(&this.size)
-	decoder.Decode(&this.hash)
-	decoder.Decode(&this.received_index)
-	decoder.Decode(&this.requested_index)
-	decoder.Decode(&this.partial_file_path)
-	decoder.Decode(&this.destination_file_path)
-
-	return nil
-}
-
-func ReturnFileList() ([]string, error) {
-	path := fmt.Sprintf("./.pft/file-list")
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
 
 // returns true if the payload was written to disk
-func (this *Download) HandleDataPacket(index uint32, payload []byte) bool {
-	if index == this.received_index + 1 {
-		file, err := os.OpenFile(this.partial_file_path, os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0600)
-		CheckError(err)
+func (this Download) HandleDataPacket(index uint32, payload []byte) bool {
+    if index != this.received_index + 1 {
+        this.requested_index = this.received_index
+        return false
+    }
 
-		n, err := file.Write(payload)
-		CheckError(err)
-		log.Printf("payload: wrote %d bytes\n", n)
-		file.Close()
+    n, err := this.local_file.Write(payload)
+    CheckError(err)
+    log.Println("wrote", n, "bytes to", this.local_file.Name())
 
-		this.received_index = index
-		return true
-	} else {
-		this.requested_index = this.received_index
-	}
-	return false
-}
-
-func (this *Download) HandleReqPacket(size uint64, hash []byte) {
-	this.size = size
-	this.hash = hash
+    this.received_index = index
+    return true
 }
 
 // saves the internal variables to a partial download file
-func (this *Download) SavePartialDownloadInfo() {
-	this.requested_index = this.received_index
-	file, err := os.Create(this.partial_file_path + ".info")
-	CheckError(err)
-	defer file.Close()
+func (this Download) CreatePartFile(path string) {
+    file, err := os.OpenFile(path, os.O_TRUNC | os.O_WRONLY | os.O_CREATE, 644)
+    CheckError(err)
+    defer file.Close()
 
-	buffer := bytes.Buffer{}
-	encoder := gob.NewEncoder(&buffer)
-	err = encoder.Encode(this)
-	CheckError(err)
+    encoder := gob.NewEncoder(file)
+    encoder.Encode(this.server)
+    encoder.Encode(this.port)
+    encoder.Encode(this.rid)
+    encoder.Encode(this.size)
+    encoder.Encode(this.hash)
+}
 
-	n, err := file.Write(buffer.Bytes())
-	CheckError(err)
-	log.Printf("info: wrote %d bytes\n", n)
+func (this Download) RemovePartFile() {
+    os.Remove(this.local_file.Name() + ".parts")
 }
 
 // loads the internal variables from a partial download file
-func LoadPartialDownload(info_file_path string) *Download {
-	data, err := ioutil.ReadFile(info_file_path)
-	CheckError(err)
+func LoadPartFile(download_file_path string, size uint64, hash []byte, d *Download) error {
+    part_file_path := download_file_path + ".part"
+    part_file, err := os.OpenFile(part_file_path, os.O_RDONLY, 644)
+    if err != nil {
+        return err
+    }
+    defer part_file.Close()
 
-	download_info := new(Download)
-	buffer := bytes.NewBuffer(data)
-	decoder := gob.NewDecoder(buffer)
-	err = decoder.Decode(&download_info)
-	CheckError(err)
+    download_file, err := os.OpenFile(download_file_path, os.O_WRONLY | os.O_APPEND, 644)
+    if err != nil {
+        return err
+    }
+    download_info, err := download_file.Stat()
+    if err != nil {
+        download_file.Close()
+        return err
+    }
+    if download_info.Size() % DATA_BLOCK_SIZE != 0 {
+        download_file.Close()
+        return errors.New("partial download file size is not divisile by data block size")
+    }
 
-	log.Println(download_info)
-	return download_info
-}
+    decoder := gob.NewDecoder(part_file)
+    decoder.Decode(d.server)
+    decoder.Decode(d.port)
+    decoder.Decode(d.rid)
+    decoder.Decode(d.size)
+    err = decoder.Decode(d.hash)
+    if err != nil {
+        download_file.Close()
+        return err
+    }
 
-func (this *Download) CheckErrorHash(reference []byte) bool {
-	return bytes.Compare(this.hash, reference) == 0
+    if d.size != size || d.hash != hash {
+        // file has changed
+        download_file.Close()
+        return errors.New("file has changed since part was downloaded")
+    }
+
+    d.received_index = download_info.Size() / DATA_BLOCK_SIZE
+    d.requested_index = download_info.Size() / DATA_BLOCK_SIZE
+    d.local_file = download_file
+
+    return nil
 }
 
 func (this *Download) IsFinished() bool {
-	return uint64(this.received_index * DATA_BLOCK_SIZE) >= this.size
-}
-
-// moves temporary "partial" download to the final destination
-// deletes info files
-func (this *Download) FinishDownload() {
-	err := os.Rename(this.partial_file_path, this.destination_file_path)
-	CheckError(err)
-	err = os.Remove(this.partial_file_path + ".info")
-	CheckError(err)
+    return uint64(this.received_index * DATA_BLOCK_SIZE) >= this.size
 }
 
 // creates get packet for requested_index + 1 (use encode function from packets.go)
-func (this *Download) CreateNextGet() []byte {
-
-	this.requested_index += 1
-	get := EncodeGet(this.requested_index)
-	log.Println(this.requested_index)
-	return get
-}
-
-// checks if partial download already exists
-func CheckIfPartiallyDownloaded(server string, port int, rid string) (bool, string) {
-	info_file_path := fmt.Sprintf("./.pft/%s:%d/%s.info", server, port, strings.Replace(rid, "/", ":", -1))
-	_, err := os.Stat(info_file_path)
-	exists := err == nil
-	return exists, info_file_path
+func (this Download) CreateNextGet() []byte {
+    this.requested_index += 1
+    get := EncodeGet(this.requested_index)
+    log.Println("requesting", this.requested_index)
+    return get
 }
 
 // initiates the download: creates the partial download file, initializes variables
-func InitDownload(server string, port int, rid string, storage_dir string) *Download {
-	server_dir := fmt.Sprintf("./.pft/%s:%d", server, port)
-	err := os.MkdirAll(server_dir, 0777)
-	CheckError(err)
+func InitDownload(server string, port int, rid string, size uint64, hash []byte) Download {
+    download_file_path := fmt.Sprintf("%s/%s", GetFileDir(), filepath.Base(rid))
+    part_file_path := download_file_path + ".part"
 
-	flat_rid := strings.Replace(rid, "/", ":", -1)
-	partial_file_path := fmt.Sprintf("%s/%s", server_dir, flat_rid)
+    var d Download
+    if err := LoadPartFile(download_file_path, size, hash, &d); err == nil {
+        // download loaded from .part file
+        return d
+    }
 
-	destination_file_path, err := getDestinationFilePath(storage_dir, rid)
-	CheckError(err)
+    file, err := os.OpenFile(download_file_path, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 644)
+    CheckError(err)
 
-	download_info := Download{
-		server:                server,
-		port:                  port,
-		rid:                   rid,
-		size:                  0,
-		hash:                  nil,
-		received_index:        0,
-		requested_index:       0,
-		partial_file_path:     partial_file_path,
-		destination_file_path: destination_file_path,
-	}
+    d.server = server
+    d.port = port
+    d.rid = rid
+    d.size = size
+    d.hash = hash
+    d.received_index = 0
+    d.requested_index = 0
+    d.local_file = file
 
-	download_info.SavePartialDownloadInfo()
-	file, err := os.Create(download_info.partial_file_path)
-	CheckError(err)
-	file.Close()
-
-	return &download_info
-}
-
-func getDestinationFilePath(storage_dir string, rid string) (string, error) {
-	path := ""
-	if strings.HasPrefix(rid, "file:") {
-		filename := rid[5:len(rid)]
-		path = fmt.Sprintf("%s/%s", storage_dir, filename)
-	} else if rid == "file-list" {
-		path = fmt.Sprintf("./.pft/file-list")
-	} else {
-		return path, errors.New("Unknown resource type")
-	}
-
-	err := os.MkdirAll(filepath.Dir(path), 0777)
-	return path, err
+    d.CreatePartFile(part_file_path)
+    return d
 }
