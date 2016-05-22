362cea89b7bb22b8e49872f29cb844fb70c9460a
Robert Brünig <rbr@iqusystems.com>
2016-05-22 19:20:26 +0200

    excluding dirs from file-list

--- pft/upload.go
+++ pft/upload.go
@@ -51,7 +51,9 @@ func getFileList(storage_dir string) []byte {
 
 	var file_names []string
     filepath.Walk(storage_dir, func(path string, f os.FileInfo, err error) error {
-        file_names = append(file_names, path)
+        if !f.IsDir() {
+            file_names = append(file_names, path)
+        }
         return nil
     })
 	files_string := strings.Join(file_names, "\n")
