293c174279fe62e7d0c1f8a97701e46dbc15cc36
Valeria Chernenko <valeriia.chernenko@tum.de>
2016-06-17 21:02:00 +0200

    - remove extra changeDir - remove anonymous functions

--- test.go
+++ test.go
@@ -41,78 +41,60 @@ func ChangeDir1(local_addr *net.UDPAddr) {
 }
 
 func runServer(port int){
-    go func() {
-        //defer wg.Done()
-        local_addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:" + strconv.Itoa(port))
-        pft.CheckError(err)
-        ChangeDir1(local_addr)
+    //defer wg.Done()
+    local_addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:" + strconv.Itoa(port))
+    pft.CheckError(err)
+    ChangeDir1(local_addr)
 
-
-        peer := pft.MakePeer(local_addr, nil) // accept packets from any remote
-        peer.Run()
-    }()
+    peer := pft.MakePeer(local_addr, nil) // accept packets from any remote
+    peer.Run()
 }
 
 func runClient(){
-    go func() {
-        for true {
-            reader := bufio.NewReader(os.Stdin)
-            fmt.Print("Request File: ")
-            file_name, _ := reader.ReadString('\n')
-            file_name = file_name[len(file_name) - 8:7] //TODO: Change to use all strings, not only seven character strings e.g. uno.txt
-            fmt.Print("file_name: ")
-            fmt.Println(file_name)
-            fmt.Print("At port of localhost: ")
-            request_port, _ := reader.ReadString('\n')
-            fmt.Println(request_port)
+    for true {
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Print("Request File: ")
+        file_name, _ := reader.ReadString('\n')
+        file_name = file_name[len(file_name) - 8:7] //TODO: Change to use all strings, not only seven character strings e.g. uno.txt
+        fmt.Print("file_name: ")
+        fmt.Println(file_name)
 
-            port, err := strconv.Atoi(request_port[len(request_port) - 5:4])
-            local_addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
-            pft.CheckError(err)
+        fmt.Print("At port of localhost: ")
+        request_port, _ := reader.ReadString('\n')
+        fmt.Println(request_port)
 
-            ChangeDir1(local_addr)
+        port, err := strconv.Atoi(request_port[len(request_port) - 5:4])
+        local_addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+        pft.CheckError(err)
 
-            server := fmt.Sprintf("%s:%d", "localhost", port)
-            server_addr, err := net.ResolveUDPAddr("udp", server)
-            pft.CheckError(err)
+        server := fmt.Sprintf("%s:%d", "localhost", port)
+        server_addr, err := net.ResolveUDPAddr("udp", server)
+        pft.CheckError(err)
 
-            peer := pft.MakePeer(local_addr, server_addr) //accept only packets from server_addr; on nil: accept packets from any remote
+        peer := pft.MakePeer(local_addr, server_addr)
 
-            // download mode
-            resource := "file-list" // download file list if no file specified
-            if file_name != "" {
-                resource = "file:" + file_name
-            }
-
-            peer.Download(resource, server_addr)
-
-            peer.Run()
-        }
-    }()
+        peer.Download("file:" + file_name, server_addr)
+        peer.Run()
+    }
 }
 
 func main() {
     ownPublishPortArg := flag.Int("p", 4455, "ownPublishPortArg")
-    //foreignLoadPortArg := flag.Int("c", 5566, "foreignLoadPortArg")
-    //consumeArg := flag.Bool("x", false, "start in server mode")
-    //downloadArg := flag.String("d", "", "file to be downloaded")
     flag.Parse()
 
-    runtime.GOMAXPROCS(2)
+    fmt.Println("Number of CPUs: ", runtime.NumCPU())
+    runtime.GOMAXPROCS(runtime.NumCPU())
 
     var wg sync.WaitGroup
     wg.Add(2)
 
     fmt.Println("Starting Go Routines")
 
-    runServer(*ownPublishPortArg)
+    go runServer(*ownPublishPortArg)
+    go runClient()
 
-    runClient()
-
-
-
-    fmt.Println("Waiting To Finish")
+    fmt.Println("Waiting to finish...")
     wg.Wait()
 
-    fmt.Println("\nTerminating Program")
+    fmt.Println("\n...terminating")
 }
