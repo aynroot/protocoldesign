1c017c9b8b422ae77e97a4ca43e6f7aeec2d7a40
Ricardo Nales <ricardo@Ricardos-MacBook-Air-10.local>
2016-06-19 14:53:06 +0200

    Fixed an issue

--- p2p/merge.go
+++ p2p/merge.go
@@ -47,6 +47,9 @@ func MergeFile(file DownloadedFile) bool {
     location := "pft-files/" + file.file_path
     fmt.Println(location)
 
+    err := os.Remove(location)
+    defer pft.CheckError(err)
+
     // open (if doesn't exists, create) file in append mode.
     merged_file, err := os.OpenFile(location, os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0644)
     pft.CheckError(err)
@@ -80,7 +83,7 @@ func MergeFile(file DownloadedFile) bool {
     merged_file.Close()
 
     // original
-    hash := tornet.CalcHash("torrent-files/test.pdf")
+    hash := tornet.CalcHash("../torrent-files/test.pdf")
     fmt.Println(hash)
 
     // new one
